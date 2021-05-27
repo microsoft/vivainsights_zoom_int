46750069f8198655cb2e1b7e874b3eddb8e26c5c
Martin Chan <martinchan53@gmail.com>
2021-05-27 15:31:19 +0100

    feat: update log file

--- Admin/script/AdminBindandHash.R
+++ Admin/script/AdminBindandHash.R
@@ -34,7 +34,8 @@ for(x in packages){
 
 # Initiate log -----------------------------------------------------------
 # Create log file location
-tmp <- file.path(tempdir(), paste0(start_t, ".log"))
+# tmp <- file.path(tempdir(), paste0(start_t, ".log"))
+tmp <- file.path(tempdir(), "log.log")
 
 # Open log
 lf <- log_open(tmp)
@@ -47,16 +48,21 @@ source("internal/stamp_time.R")
 
 #### ZOOM ADMIN -----------------------------------------------------------
 
-logr::log_print("Listing files from Zoom...")
+logr::log_print("Listing files from Zoom...", console = FALSE)
 
 # Read in file names from `../input/zoom_reports`
 input_files <- list.files("../input/zoom_reports")
 
-logr::log_print(input_files)
+# Match csv files only
+input_files <- input_files[grepl(pattern = "csv$",
+                                 x = input_files,
+                                 ignore.case = TRUE)]
+
+logr::log_print(input_files, console = FALSE)
 
 # Read mapping file -------------------------------------------------------
 
-logr::log_print("Reading in mapping file...")
+logr::log_print("Reading in mapping file...", console = FALSE)
 
 path_map <- list.files("../input/") %>%
   .[grepl(pattern = "mapping file",
@@ -72,7 +78,7 @@ if(length(path_map) == 0){
   full_path_map <- paste0("../input/", path_map)
 }
 
-logr::log_print("full_path_map")
+logr::log_print(full_path_map, console = FALSE)
 
 # Output is assigned to `zoom_hashed` -------------------------------------
 
@@ -93,7 +99,9 @@ zoom_hashed %>%
       path_zoom_hashed
   )
 
-message(paste("Hashed Zoom file saved to", path_zoom_hashed))
+# message(paste("Hashed Zoom file saved to", path_zoom_hashed))
+logr::log_print(paste("Hashed Zoom file saved to", path_zoom_hashed), console = TRUE)
+
 message("Please send the hashed file to Workplace Analytics Analyst.")
 
 # ZOOM ADMIN SENDS OUTPUT HASH FILE TO ANALYST
@@ -101,3 +109,7 @@ message("Please send the hashed file to Workplace Analytics Analyst.")
 # Termination message ----------------------------------------------------
 
 message("Run complete. ", stamp_time(start_t, unit = "mins"))
+
+# Print log --------------------------------------------------------------
+
+writeLines(text = readLines(lf), "../output/log.log")
