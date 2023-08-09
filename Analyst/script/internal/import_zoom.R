2b89acbdbdf4508d2431399ed95707345b9db1e6
Martin Chan <martinchan53@gmail.com>
2023-08-09 10:53:47 +0100

    chore: update variable renaming

--- Analyst/script/internal/import_zoom.R
+++ Analyst/script/internal/import_zoom.R
@@ -17,30 +17,35 @@ clean_zoom <- function(x, date_format = "%m/%d/%Y %H:%M:%S"){
     # Remove blanks -------------------------------------------------------
   dplyr::filter(
     !is.na(Topic),
-    !is.na(`Meeting ID`),
-    !is.na(`User Name`)
+    !is.na(`MeetingID`),
+    !is.na(`User_Name`)
   ) %>%
     set_names(gsub(pattern = " ", replacement = "_", x = names(.))) %>%
     set_names(gsub(pattern = "[?]|\\(|\\)", replacement = "", x = names(.))) %>%
     set_names(gsub(pattern = "[...]", replacement = "", x = names(.))) %>%
-    dplyr::rename(
-      User_Email_2 = "User_Email_1",
-      User_Email_1 = "User_Email",
-      Duration_Minutes_2 = "Duration_Minutes_1",
-      Duration_Minutes_1 = "Duration_Minutes"
-    )  %>%
+#    dplyr::rename(
+#     User_Email_2 = "User_Email_1",
+#      User_Email_1 = "User_Email",
+#      Duration_Minutes_2 = "Duration_Minutes_1",
+#      Duration_Minutes_1 = "Duration_Minutes"
+#    )  %>%
+	
+	# Remove blank participants --------------------------------------------
+	# This represents unusable data
+	# Participants is itself a column available in the data
+	
+	filter(!is.na(User_Email_2)) %>%
 
     # Enforce variable type ------------------------------------------------
-  mutate(
-    across(
-      .cols =
-        c(Join_Time,
-          Leave_Time,
-          Creation_Time,
-          Start_Time,
-          End_Time),
-      # .fns = ~lubridate::parse_date_time(x = ., orders = date_format)
-      .fns = ~as.POSIXct(x = ., format = date_format, tz = "UTC")
-    )
-  )
+	  mutate(
+		across(
+		  .cols =
+			c(Join_Time,
+			  Leave_Time,
+			  Creation_Time,
+			  Start_Time,
+			  End_Time),
+		  .fns = ~as.POSIXct(x = ., format = date_format, tz = "UTC")
+		)
+	  )
 }
